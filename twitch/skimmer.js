16838b77f4b458fc71d3884f613d214c9b512e54
Paul E Lederer <plederer93@gmail.com>
2017-02-07 11:42:22 -0600

    got the skimmer working as in-browser code

--- twitch/skimmer.js
+++ twitch/skimmer.js
@@ -1,11 +1,9 @@
-const tmi = require('tmi.js');//will need to be removed before production
-const twitch = require('../hidden/twitch');//this is where we are getting info
-const LocalStorage = require('node-localstorage').LocalStorage;//remove this before production
-sessionStorage = new LocalStorage('./scratch');//remove this too
-const username = twitch.username;
-const oauthKey = twitch.oauthKey;
-const channel = twitch.channel;
-const options = {
+(function(){
+const username = twitch.username;//this needs be the username
+const oauthKey = docCookies.getItem('token');
+const channel = twitch.channel;//this needs to be the channel name
+const options =
+{
     options: {
         debug: true
     },
@@ -22,24 +20,15 @@ const client = new tmi.client(options);
 client.connect();
 let messages = []
 client.on('message', (channel, userstate, message, self) => {
-    // Don't listen to my own messages..
-    if (self) return;
 
     // Handle different message types..
     switch(userstate["message-type"]) {
-        case "action":
-            // This is an action message..
-            break;
         case "chat":
             messages.push(message)
             sessionStorage.setItem('messages', messages);
-            // This is a chat message..
-            break;
-        case "whisper":
-            // This is a whisper..
-            break;
         default:
             // Something else ?
             break;
     }
 });
+}())
