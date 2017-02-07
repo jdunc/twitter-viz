f18b2ee6a50f830cb0095799158b467708117071
Paul E Lederer <plederer93@gmail.com>
2017-02-07 14:00:34 -0600

    testing twitch API calls

--- public/twitch/skimmer.js
+++ public/twitch/skimmer.js
@@ -1,6 +1,15 @@
 (function(){
-const username = twitch.username;//this needs be the username
-const oauthKey = docCookies.getItem('token');
+const token = JSON.parse(docCookies.getItem('token').substr(2)).access_token;
+const username = $.ajax('https://api.twitch.tv/kraken/user',
+{
+  headers:
+  {
+    'Accept': 'application/vnd.twitchtv.v3+json',
+    'Authorization': `OAuth ${token}`,
+    'Client-ID': 'ipr78n41sqd6fqbacj2gykwv5lxpnd',
+  }
+}).done((data)=>console.log(data, 'yay'););//this needs be the username
+const password = 'oauth:' + token;
 const channel = twitch.channel;//this needs to be the channel name
 const options =
 {
@@ -12,7 +21,7 @@ const options =
     },
     identity: {
         username: username,
-        password: oauthKey
+        password: password
     },
     channels: [`#${channel}`]
 };
