2a99cbd6b98108f4d965d7cab64be098bb88d352
C S Harding <charliesharding@gmail.com>
2017-02-08 11:39:27 -0600

    converted iso date format to milliseconds

--- public/twitch/chat.js
+++ public/twitch/chat.js
@@ -2,21 +2,20 @@
     function chatGrabber(timeRange) {
         console.log('ja;lsdfkjalsdfja');
         // when called, chatGrabber should return an array filled with messages that match the timeRange parameter (in miliseconds)
-        let messages = JSON.parse(sessionStorage.getItem('twitchMessages'));
-        if (messages !== null) {
-            let times = messages.map((elem)=>{
-                return elem.time
-            })
-            console.log(times[0]);
-        }
+        let messages = JSON.parse(sessionStorage.getItem('twitchMessages')) || [];
+        let times = messages.map((elem) => {
+            return elem.time
+        })
+        // new Date is used to convert the ISO formatted date back into a javascript date object, allowing the getTime method to be used
+        console.log(new Date(times[0]).getTime());
+
 
         // mapWordData(recentMessages)
     }
 
     let timeRange = 5000;
     chatGrabber(timeRange)
-    setInterval(()=>{
-        console.log('in set interval');
+    setInterval(() => {
         chatGrabber(timeRange)
     }, timeRange);
 }())
