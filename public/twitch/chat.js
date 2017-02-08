7159e80ccd3a3cd6b7dc4d55757a4bf6e9c52476
C S Harding <charliesharding@gmail.com>
2017-02-08 13:16:39 -0600

    chatgrabber returns all messages in past x miliseconds

--- public/twitch/chat.js
+++ public/twitch/chat.js
@@ -1,21 +1,29 @@
 (function() {
     function chatGrabber(timeRange) {
-        console.log('ja;lsdfkjalsdfja');
         // when called, chatGrabber should return an array filled with messages that match the timeRange parameter (in miliseconds)
         let messages = JSON.parse(sessionStorage.getItem('twitchMessages')) || [];
         let times = messages.map((elem) => {
-            return elem.time
+            return new Date(elem.time).getTime()
         })
         // new Date is used to convert the ISO formatted date back into a javascript date object, allowing the getTime method to be used
-        console.log(new Date(times[0]).getTime());
-
-
+        let lastMessage = times[times.length-1];
+        let lastMessages = [];
+        // goes through times array starting at end (most recent) and adds all messages with corresponding index
+        for (let i = times.length-1; i > -1; i--) {
+            if (lastMessage - times[i] <= timeRange) {
+                lastMessages.push(messages[i].message)
+            } else {
+                break
+            }
+        }
+        console.log(lastMessages);
         // mapWordData(recentMessages)
     }
 
+    // every second, chatGrabber returns array of messages sent in past 5 seconds
     let timeRange = 5000;
     chatGrabber(timeRange)
     setInterval(() => {
         chatGrabber(timeRange)
-    }, timeRange);
+    }, 1000);
 }())
