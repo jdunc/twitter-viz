ea850e69197e90c34c0c547121296fa683a54dd2
Jordan Duncan <j.duncan@rochester.edu>
2017-02-09 13:11:43 -0600

    added getImages route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const port = process.env.PORT || 8000;
 const getTweets = require('./routes/getTweets');
 const getTweetData = require('./routes/getTweetData');
+const getTweetImages = require('./routes/getTweetImages');
 const twitch = require('./routes/twitch');
 const home = require('./routes/home');
 var app = express();
@@ -12,6 +13,7 @@ app.set('view engine', 'ejs');
 app.use(home);
 app.use(getTweets);
 app.use(getTweetData);
+app.use(getTweetImages);
 app.use('/twitch', twitch);
 app.listen(port);
 
