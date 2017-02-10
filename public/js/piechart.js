4717a1ac2e485d77ca05aa1a3abecb208a6ed3e5
Jordan Duncan <j.duncan@rochester.edu>
2017-02-09 20:36:13 -0600

    chart loads on search

--- public/js/piechart.js
+++ public/js/piechart.js
@@ -14,40 +14,42 @@ $(document).ready(function() {
                 freq.push(results.text[i][1]);
             }
             console.log("these are the words", words, freq);
+            var ctx = document.getElementById("twitterChart");
+            var myChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: words,
+                    datasets: [{
+                        label: 'Most Common Words',
+                        data: freq,
+                        backgroundColor: [
+                            'rgba(208, 25, 25, 0.7)',
+                            'rgba(13, 17, 246, 0.7)',
+                            'rgba(246, 239, 28, 0.7)',
+                            'rgba(28, 182, 11, 0.7)',
+                            'rgba(99, 6, 238, 0.7)',
+                            'rgba(238, 145, 6, 0.7)',
+                            'rgba(194, 251, 156, .7)',
+                            'rgba(6, 238, 234, .7)',
+                            'rgba(238, 6, 238, .7)',
+                            'rgba(45, 69, 102, 1)'
+                        ],
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            }); // end new chart
         });
 
-        var ctx = document.getElementById("twitterChart");
-        var myChart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: words,
-                datasets: [{
-                    label: 'Most Common Words',
-                    data: freq,
-                    backgroundColor: [
-                        'rgba(208, 25, 25, 0.7)',
-                        'rgba(13, 17, 246, 0.7)',
-                        'rgba(246, 239, 28, 0.7)',
-                        'rgba(28, 182, 11, 0.7)',
-                        'rgba(99, 6, 238, 0.7)',
-                        'rgba(238, 145, 6, 0.7)',
-                        'rgba(194, 251, 156, .7)',
-                        'rgba(6, 238, 234, .7)',
-                        'rgba(238, 6, 238, .7)',
-                        'rgba(45, 69, 102, 1)'
-                    ],
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        }); // end new chart
+
+
 
     });
 })
